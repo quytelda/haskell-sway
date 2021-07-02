6ec1e248f93b03e7e7534f4b064165e9c8b87021
Quytelda Kahja <quytelda@tamalin.org>
2021-07-01 19:59:16 -0700

    Add lookup tables for IPC message type codes.

--- src/System/Desktop/Sway/Types.hs
+++ src/System/Desktop/Sway/Types.hs
@@ -31,6 +31,38 @@ data EventType = Workspace
                | Input
                deriving (Show)
 
+msgCodes :: [(MessageType, Word32)]
+msgCodes =
+  [ (RunCommand,      0x00)
+  , (GetWorkspaces,   0x01)
+  , (Subscribe,       0x02)
+  , (GetOutputs,      0x03)
+  , (GetTree,         0x04)
+  , (GetMarks,        0x05)
+  , (GetBarConfig,    0x06)
+  , (GetVersion,      0x07)
+  , (GetBindingModes, 0x08)
+  , (GetConfig,       0x09)
+  , (SendTick,        0x0a)
+  , (Sync,            0x0b)
+  , (GetBindingState, 0x0c)
+  , (GetInputs,       0x64)
+  , (GetSeats,        0x65)
+  ]
+
+evtCodes :: [(EventType, Word32)]
+evtCodes =
+  [ (Workspace,       0x80000000)
+  , (Mode,            0x80000002)
+  , (Window,          0x80000003)
+  , (BarConfigUpdate, 0x80000004)
+  , (Binding,         0x80000005)
+  , (Shutdown,        0x80000006)
+  , (Tick,            0x80000007)
+  , (BarStateUpdate,  0x80000014)
+  , (Input,           0x80000015)
+  ]
+
 data Message = Message MessageType ByteString
              | Event   EventType   ByteString
              deriving (Show)
