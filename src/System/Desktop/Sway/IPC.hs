16170b4af855e503d8154e9e74f04f26202642e8
Quytelda Kahja <quytelda@tamalin.org>
2021-07-02 23:05:49 -0700

    Implement connecting to the sway socket.

--- src/System/Desktop/Sway/IPC.hs
+++ src/System/Desktop/Sway/IPC.hs
@@ -2,9 +2,28 @@
 
 module System.Desktop.Sway.IPC where
 
+import           Control.Exception  (bracket)
 import           Control.Monad
 import           Network.Socket
 import           System.Environment (lookupEnv)
 
+-- | Find the path to the sway socket file.
 getSocketAddr :: IO (Maybe SockAddr)
 getSocketAddr = liftM SockAddrUnix <$> lookupEnv "SWAYSOCK"
+
+-- | Connect to a UNIX domain socket at the given file path.
+-- The socket must exist.
+openUnixSocket :: FilePath -> IO Socket
+openUnixSocket path = do
+  sock <- socket AF_UNIX Stream defaultProtocol
+  connect sock (SockAddrUnix path)
+  return sock
+
+-- | Close the connection with a UNIX socket.
+-- Throws an error on failure.
+closeUnixSocket :: Socket -> IO ()
+closeUnixSocket = close'
+
+-- | Execute an action that makes use of a UNIX socket at the given path.
+withUnixSocket :: FilePath -> (Socket -> IO a) -> IO a
+withUnixSocket path = bracket (openUnixSocket path) closeUnixSocket
