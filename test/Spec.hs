973d7192c97e4aa31ea5c578262bf3ede2737329
Quytelda Kahja <quytelda@tamalin.org>
2021-07-02 22:46:31 -0700

    Add unit tests for serializing/deserializing Message data.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Data.Binary.Get
+import           Data.Binary.Put
+import qualified Data.ByteString.Lazy      as BL
+import           Test.Hspec
+
+import           System.Desktop.Sway.Types
+
+commandExitMsg = Message RunCommand "exit"
+commandExitBin = BL.pack [ 0x69, 0x33, 0x2d, 0x69
+                         , 0x70, 0x63, 0x04, 0x00
+                         , 0x00, 0x00, 0x00, 0x00
+                         , 0x00, 0x00, 0x65, 0x78
+                         , 0x69, 0x74
+                         ]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Message" $ do
+    it "can encode Messages to binary" $
+      runPut (putMessage commandExitMsg) `shouldBe` commandExitBin
+
+    it "can decode Messages from binary" $
+      runGet getMessage commandExitBin `shouldBe` commandExitMsg
