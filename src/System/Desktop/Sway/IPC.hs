27d2eb55091b7917b2ad34cc02402dc76399d838
Quytelda Kahja <quytelda@tamalin.org>
2021-07-02 23:01:00 -0700

    Implement finding the sway socket.

--- src/System/Desktop/Sway/IPC.hs
+++ src/System/Desktop/Sway/IPC.hs
@@ -1,1 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module System.Desktop.Sway.IPC where
+
+import           Control.Monad
+import           Network.Socket
+import           System.Environment (lookupEnv)
+
+getSocketAddr :: IO (Maybe SockAddr)
+getSocketAddr = liftM SockAddrUnix <$> lookupEnv "SWAYSOCK"
