d639c830c36efb625ba5bbfd7650af35ca94df06
Quytelda Kahja <quytelda@tamalin.org>
2021-07-07 06:31:41 -0700

    Add unit tests for parseResults.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,8 +2,9 @@
 
 import           Data.Binary.Get
 import           Data.Binary.Put
-import           Data.ByteString.Lazy      (ByteString)
-import qualified Data.ByteString.Lazy      as BL
+import           Data.ByteString.Lazy       (ByteString)
+import qualified Data.ByteString.Lazy       as BL
+import qualified Data.ByteString.Lazy.Char8 as Char8
 import           Data.IORef
 import           Test.Hspec
 
@@ -16,6 +17,25 @@ commandExitBin = BL.pack [ 0x69, 0x33, 0x2d, 0x69
                          , 0x00, 0x00, 0x65, 0x78
                          , 0x69, 0x74
                          ]
+
+failureParse :: ByteString
+failureParse = "Unknown/invalid command 'wrong'"
+
+resultSuccess = "{ \"success\": true }"
+resultFailure = mconcat [ "{ \"success\": false"
+                        , ", \"parse_error\": true"
+                        , ", \"error\": \"" <> failureParse <> "\" "
+                        , "}"
+                        ]
+
+reply_RunCommand_1 :: ByteString
+reply_RunCommand_1 = "[ " <> resultSuccess <> " ]"
+
+reply_RunCommand_2 :: ByteString
+reply_RunCommand_2 = mconcat [ "[ " <> resultSuccess
+                             , ", " <> resultFailure <> " "
+                             , "]"
+                             ]
 
 -- | Mocket is a mock socket for testing IPC socket operations.
 newtype Mocket = Mocket (IORef ByteString)
@@ -32,6 +52,13 @@ main = hspec $ do
 
     it "can decode Messages from binary" $
       msgDecode commandExitBin `shouldBe` Right commandExitMsg
+
+  describe "parseResults" $ do
+    it "should succeed if every command succeeded" $
+      parseResults reply_RunCommand_1 `shouldBe` Right ()
+    it "should fail if any command failed" $
+      let err = "Error in $: parse error: " <> Char8.unpack failureParse
+      in parseResults reply_RunCommand_2 `shouldBe` Left err
 
   describe "SwayT" $ do
     it "can send raw bytes" $ do
