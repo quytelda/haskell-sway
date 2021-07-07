828c22e9fa31a5c46e9ce81dfb1f69f2d7c1f37e
Quytelda Kahja <quytelda@tamalin.org>
2021-07-07 06:57:43 -0700

    Move RUN_COMMAND functionality to Command module.

---
+++ src/System/Desktop/Sway/Command.hs
@@ -0,0 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module System.Desktop.Sway.Command where
+
+import           Control.Monad
+import           Control.Monad.Trans        (MonadIO)
+import           Control.Monad.Trans.Except (except, throwE)
+import           Data.Aeson
+import           Data.Aeson.Types           (Parser, parseEither)
+import           Data.ByteString.Lazy       (ByteString)
+
+import           System.Desktop.Sway.Types
+
+-- | Parse an error message from a sway command failure result JSON object.
+parseFailure :: Object -> Parser String
+parseFailure obj = do
+  parseError <- obj .: "parse_error"
+  errorMsg   <- obj .: "error"
+  return $ (parseError ? "parse error: ") <> errorMsg
+
+-- | Parse a sway command result JSON object.
+-- For success results, return `()`.
+-- For failure results, parse error information and fail appropriately.
+parseSuccess :: Value -> Parser ()
+parseSuccess = withObject "command result" $ \obj -> do
+  success <- obj .: "success"
+  unless success $
+    fail =<< parseFailure obj
+
+-- | Parse a `RUN_COMMAND` reply payload, which is an array of objects
+-- indicating the respective success or failure of each command sent.
+parseResults :: ByteString -> Either String ()
+parseResults bytes = eitherDecode bytes >>= parseEither (overArray parseSuccess)
+  where
+    overArray = withArray "list of results" . mapM_
+
+-- | Run a sway command.
+-- Send a RUN_COMMAND IPC message and return the reply payload.
+runCommand :: (MonadIO m, SendRecv s) => ByteString -> SwayT s m ()
+runCommand cmd = do
+  reply <- ipc $ Message RunCommand cmd
+  case reply of
+    Message RunCommand payload -> except $ parseResults payload
+    _                          -> throwE "expected RUN_COMMAND reply"
+
--- src/System/Desktop/Sway/Types.hs
+++ src/System/Desktop/Sway/Types.hs
@@ -7,8 +7,6 @@ import           Control.Monad
 import           Control.Monad.Trans            (MonadIO, lift, liftIO)
 import           Control.Monad.Trans.Except
 import           Control.Monad.Trans.Reader
-import           Data.Aeson
-import           Data.Aeson.Types (Parser, parseEither)
 import           Data.Binary.Get
 import           Data.Binary.Put
 import           Data.ByteString.Lazy           (ByteString)
@@ -205,35 +203,3 @@ ipc msg = sendMessage msg >> recvMessage
 (?) :: Monoid p => Bool -> p -> p
 True  ? m = m
 False ? _ = mempty
-
--- | Parse an error message from a sway command failure result JSON object.
-parseFailure :: Object -> Parser String
-parseFailure obj = do
-  parseError <- obj .: "parse_error" :: Parser Bool
-  errorMsg   <- obj .: "error" :: Parser String
-  return $ (parseError ? "parse error: ") <> errorMsg
-
--- | Parse a sway command result JSON object.
--- For success results, return `()`.
--- For failure results, parse error information and fail appropriately.
-parseSuccess :: Value -> Parser ()
-parseSuccess = withObject "command result" $ \obj -> do
-  success <- obj .: "success"
-  unless success $
-    fail =<< parseFailure obj
-
--- | Parse a `RUN_COMMAND` reply payload, which is an array of objects
--- indicating the respective success or failure of each command sent.
-parseResults :: ByteString -> Either String ()
-parseResults bytes = eitherDecode bytes >>= parseEither (overArray parseSuccess)
-  where
-    overArray = withArray "list of results" . mapM_
-
--- | Run a sway command.
--- Send a RUN_COMMAND IPC message and return the reply payload.
-runCommand :: (MonadIO m, SendRecv s) => ByteString -> SwayT s m ()
-runCommand cmd = do
-  reply <- ipc $ Message RunCommand cmd
-  case reply of
-    Message RunCommand payload -> except $ parseResults payload
-    _                          -> throwE "expected RUN_COMMAND reply"
