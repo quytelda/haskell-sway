d1c0c8be0d6e8cc4a1d20a360604869dbc68c253
Quytelda Kahja <quytelda@tamalin.org>
2021-07-08 01:35:59 -0700

    Move getWorkspaces to Workspace module.

---
+++ src/System/Desktop/Sway/Workspaces.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module System.Desktop.Sway.Workspaces where
+
+import           Control.Monad.Trans        (MonadIO)
+import           Control.Monad.Trans.Except (except, throwE)
+import           Data.Aeson                 (eitherDecode)
+
+import           System.Desktop.Sway.Types
+
+-- | Get the list of sway workspaces.
+-- Send a `GET_WORKSPACES` IPC message and return the parsed results.
+getWorkspaces :: (MonadIO m, SendRecv s) => SwayT s m [Workspace]
+getWorkspaces = do
+  reply <- ipc $ Message GetWorkspaces ""
+  case reply of
+    Message GetWorkspaces payload -> except $ eitherDecode payload
+    _                             -> throwE "expected GET_WORKSPACES reply"
