760c91603c29a53c297aaff78c92115157ae657b
Quytelda Kahja <quytelda@tamalin.org>
2021-07-09 11:46:27 -0700

    Update unit test name after renaming function.

--- test/System/Desktop/Sway/CommandSpec.hs
+++ test/System/Desktop/Sway/CommandSpec.hs
@@ -32,7 +32,7 @@ reply_RunCommand_failure = encode [ resultSuccess_json
 
 spec :: Spec
 spec = describe "Command" $ do
-  describe "parseResults" $ do
+  describe "decodeResults" $ do
     context "when provided valid input" $ do
       it "should succeed when all commands succeeded" $
         decodeResults reply_RunCommand_success `shouldBe` Right ()
