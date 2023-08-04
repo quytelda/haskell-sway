15d05fcdecf70784f8257042d0f35893f9c66f50
Quytelda Kahja <quytelda@tamalin.org>
2023-08-04 12:44:40 -0700

    Implement GET_TREE IPC queries.

---
+++ src/System/Desktop/Sway/Tree.hs
@@ -0,0 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module System.Desktop.Sway.Tree where
+
+import           Control.Monad.Trans        (MonadIO)
+
+import           System.Desktop.Sway.Types
+import           System.Desktop.Sway.IPC
+
+-- | Get the current layout tree.
+-- Send a `GET_TREE` IPC message and return the parsed result.
+getTree :: (MonadIO m, SendRecv s) => SwayT s m Node
+getTree = query GetTree ""
