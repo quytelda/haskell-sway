40aeadbfe7e223e2546ae54f6fb5c3d24292f1e9
Quytelda Kahja <quytelda@tamalin.org>
2023-08-21 04:14:40 -0700

    Implement GET_INPUTS.

--- src/System/Desktop/Sway/Input.hs
+++ src/System/Desktop/Sway/Input.hs
@@ -15,3 +15,33 @@ getBindingModes = query GetBindingModes ""
 
 getBindingState :: (MonadIO m, SendRecv s) => SwayT s m String
 getBindingState = query GetBindingState "" >>= parseSway (.: "name")
+
+data Input = Input { inputIdentifier           :: String
+                   , inputName                 :: String
+                   , inputVendor               :: Int
+                   , inputProduct              :: Int
+                   , inputType                 :: String
+                   , inputXKBActiveLayoutName  :: Maybe String
+                   , inputXKBLayoutNames       :: Maybe [String]
+                   , inputXKBActiveLayoutIndex :: Maybe Int
+                   , inputScrollFactor         :: Maybe Double
+                   , inputLibinput             :: Maybe Object
+                   } deriving (Eq, Show)
+
+instance FromJSON Input where
+  parseJSON = withObject "Input" $ \obj -> do
+    inputIdentifier           <- obj .: "identifier"
+    inputName                 <- obj .: "name"
+    inputVendor               <- obj .: "vendor"
+    inputProduct              <- obj .: "product"
+    inputType                 <- obj .: "type"
+    inputXKBActiveLayoutName  <- obj .:? "xkb_active_layout_name"
+    inputXKBLayoutNames       <- obj .:? "xkb_layout_names"
+    inputXKBActiveLayoutIndex <- obj .:? "xkb_active_layout_index"
+    inputScrollFactor         <- obj .:? "scroll_factor"
+    inputLibinput             <- obj .:? "libinput"
+
+    return Input{..}
+
+getInputs :: (MonadIO m, SendRecv s) => SwayT s m [Input]
+getInputs = query GetInputs ""
