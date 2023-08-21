c1069e6d0861e0eff06ccb098b826fcf6741d547
Quytelda Kahja <quytelda@tamalin.org>
2023-08-21 03:52:13 -0700

    Implement GET_VERSION message.

---
+++ src/System/Desktop/Sway/Version.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
+
+module System.Desktop.Sway.Version where
+
+import           Control.Monad.Trans         (MonadIO)
+import           Data.Aeson
+
+import           System.Desktop.Sway.IPC
+import           System.Desktop.Sway.Message
+import           System.Desktop.Sway.Types
+
+data SwayVersion = SwayVersion { versionMajor         :: Int
+                               , versionMinor         :: Int
+                               , versionPatch         :: Int
+                               , versionHumanReadable :: String
+                               , versionConfigPath    :: FilePath
+                               } deriving (Eq, Show)
+
+instance FromJSON SwayVersion where
+  parseJSON = withObject "SwayVersion" $ \obj -> do
+    versionMajor         <- obj .: "major"
+    versionMinor         <- obj .: "minor"
+    versionPatch         <- obj .: "patch"
+    versionHumanReadable <- obj .: "human_readable"
+    versionConfigPath    <- obj .: "loaded_config_file_name"
+
+    return SwayVersion{..}
+
+getVersion :: (MonadIO m, SendRecv s) => SwayT s m SwayVersion
+getVersion = query GetVersion ""
