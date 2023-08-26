7351273532495449d287f93871dff2079d6c8405
Quytelda Kahja <quytelda@tamalin.org>
2023-08-26 16:54:51 -0700

    Improve haddock documentation comments.

--- src/System/Desktop/Sway/IPC.hs
+++ src/System/Desktop/Sway/IPC.hs
@@ -1,6 +1,12 @@
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 
+{-|
+Description : Types and functions for communicating with the sway socket.
+
+The sway IPC protocol uses a UNIX socket to communicate with sway as
+well as other clients.
+-}
 module System.Desktop.Sway.IPC where
 
 import           Control.Exception             (bracket)
@@ -15,49 +21,56 @@ import           System.Desktop.Sway.Exception hiding (fromString)
 import           System.Desktop.Sway.Message
 import           System.Desktop.Sway.Types
 
--- | Find the path to the sway socket file.
--- The path is stored in the $SWAYSOCK environment variable.
+-- | Find the path to the sway socket, which is a UNIX socket file.
+-- The socket path is stored in the @SWAYSOCK@ environment variable.
+--
+-- The @sway --get-socketpath@ command also returns the socket path,
+-- but as of sway version 1.8.1, the command just reads the @SWAYSOCK@
+-- variable.
 findSwaySocket :: IO (Maybe FilePath)
 findSwaySocket = lookupEnv "SWAYSOCK"
 
 -- | Connect to a UNIX domain socket at the given file path.
--- The socket must exist.
+-- A new `Socket` object will be created and returned.
+-- The socket file must exist, otherwise an error will be thrown.
 openUnixSocket :: FilePath -> IO Socket
 openUnixSocket path = do
   sock <- socket AF_UNIX Stream defaultProtocol
   connect sock (SockAddrUnix path)
   return sock
 
--- | Close the connection with a UNIX socket.
+-- | Close a UNIX socket connection.
 -- Throws an error on failure.
 closeUnixSocket :: Socket -> IO ()
 closeUnixSocket = close'
 
--- | Execute an action that makes use of a UNIX socket at the given path.
+-- | Execute an action that makes use of a UNIX socket at the given
+-- path. The socket will be safely closed at the end regardless of
+-- whether the operation was successful.
 withUnixSocket :: FilePath -> (Socket -> IO a) -> IO a
 withUnixSocket path = bracket (openUnixSocket path) closeUnixSocket
 
 -- | Execute an action that uses the system sway socket.
--- Fails if the sway socket cannot be located.
+-- If the sway socket cannot be located, raise an error.
 withSwaySocket :: (Socket -> IO a) -> IO a
 withSwaySocket f = findSwaySocket >>= \case
   Just path -> withUnixSocket path f
   Nothing   -> fail "Unable to get sway socket path."
 
--- | Send bytes using the connection object within the monad.
+-- | Send bytes to the sway socket.
 sendBytes :: SendRecv s m => ByteString -> SwayT s m ()
 sendBytes bytes = getConnection >>= lift . flip send bytes
 
--- | Receive bytes using the connection object within the monad.
+-- | Receive bytes from the sway socket.
 recvBytes :: SendRecv s m => SwayT s m ByteString
 recvBytes = getConnection >>= lift . recv
 
--- | Send a Message using the connection object within the monad.
+-- | Send a `Message` over the sway socket.
 sendMessage :: SendRecv s m => Message -> SwayT s m ()
 sendMessage = sendBytes . msgEncode
 
--- | Receive a Message using the connection object within the monad.
--- Throws an exception if the received message cannot be parsed.
+-- | Receive a `Message` from the sway socket.
+-- Throws an exception if the message parsing fails.
 recvMessage :: (MonadError e m, FromString e, SendRecv s m) => SwayT s m Message
 recvMessage = recvBytes >>= eitherToSway . msgDecode
 
